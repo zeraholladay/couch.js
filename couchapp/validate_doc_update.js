05b62b23cb1cfa726c1440e705b35f536c62fc1b
zeraholladay <zeraholladay@gmail.com>
2012-08-17 14:19:45 -0400

    fixed issue with validate_doc_update not working on deletes

--- couchapp/validate_doc_update.js
+++ couchapp/validate_doc_update.js
@@ -1,5 +1,8 @@
 function(newDoc, oldDoc, userCtx, secObj) {
+    if (((oldDoc && (oldDoc.user == userCtx.name))) && newDoc._deleted) {
+        return true;
+    }
     if (newDoc.user !== userCtx.name) {
-        throw({forbidden: 'Action prohibited'});
+        throw({forbidden: 'Action prohibited by validate_doc_update.js'});
     }
 }
